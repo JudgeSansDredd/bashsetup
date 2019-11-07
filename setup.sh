c98a4dc1cc3d30ab1925fa5645ff3868bd86eac7
JudgeSansDredd <nathan.stanley@dtn.com>
2019-11-07 14:21:36 -0500

    Updates setup with new items

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
-# Switch to zshell if not already there
-chsh -s /bin/zsh
+######################
+# Setup Oh My Zshell #
+######################
 
-# Install Oh My Zshell
+# Install oh my zshell (this will ask to switch shells if you have not
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install syntax highlighting
@@ -13,13 +14,20 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # Copy to ~/.zshrc
 cp ./.zshrc ~/.zshrc
 
+####################################
+# Brew install other terminal apps #
+####################################
+
 # Brew install command line tools
-brew install tldr diff-so-fancy bat exa
+brew install tldr diff-so-fancy bat fzf
+
+# Setup fzf hotkeys and such
+/usr/local/opt/fzf/install
 
 # Configure git to use diff-so-fancy
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
-# Add colors to git diff
+# Configure git to use different colors for diffs
 git config --global color.ui true
 
 git config --global color.diff-highlight.oldNormal    "red bold"
@@ -33,3 +41,8 @@ git config --global color.diff.commit     "yellow bold"
 git config --global color.diff.old        "red bold"
 git config --global color.diff.new        "green bold"
 git config --global color.diff.whitespace "red reverse"
+
+##########
+# Vim RC #
+##########
+cp ~/.bash/.vimrc ~/.vimrc
