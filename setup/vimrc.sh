d58b010c528734e6305c58100fb5109f60d6bcc5
Nathan Stanley <nstanley@bandwidth.com>
2025-04-04 14:54:53 -0400

    Remove vimrc and zshrc when symlinking

--- setup/vimrc.sh
+++ setup/vimrc.sh
@@ -3,5 +3,6 @@
 source $HOME/.bash/setup/whiptail.sh
 
 if (whiptail --yesno "Link .vimrc to this machine?" --title "Vimrc" $BOX_HEIGHT $BOX_WIDTH); then
+    [ -e $HOME/.vimrc ] && rm -f $HOME/.vimrc
     ln -nsf $HOME/.bash/configs/.vimrc $HOME/.vimrc
 fi
--- setup/zshrc.sh
+++ setup/zshrc.sh
@@ -3,5 +3,6 @@
 source $HOME/.bash/setup/whiptail.sh
 
 if (whiptail --yesno "Link .zshrc to home directory?" --title ".zshrc" $BOX_HEIGHT $BOX_WIDTH); then
+    [ -e $HOME/.zshrc ] && rm -f $HOME/.zshrc
     ln -nsf $HOME/.bash/configs/.zshrc $HOME/.zshrc
 fi
